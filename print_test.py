aa190e4d63a32b6b48739035487fb00564f8fbb0
i-am-misaki <mkmtnm@gmail.com>
2023-12-12 15:11:42 +0900

    add 2print-test files

---
+++ print_test.py
@@ -0,0 +1,9 @@
+'''
+Created on 2023/12/12
+
+@author: 7d24
+
+'''
+print("hello world")
+print("cloudy day")
+print("日本語")
---
+++ print_testAgain.py
@@ -0,0 +1,7 @@
+'''
+Created on 2023/12/12
+
+@author: 7d24
+'''
+print("hello again")
+print("日本語")
